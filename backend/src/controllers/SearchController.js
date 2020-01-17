c8f790c499c46230aebe8157e0993d9559b8db09
vhnakai <36956647+vhnakai@users.noreply.github.com>
2020-01-17 05:51:00 -0300

    mobile

--- backend/src/controllers/SearchController.js
+++ backend/src/controllers/SearchController.js
@@ -4,14 +4,16 @@ const parseStringAsArray = require('../utils/parseStringAsArray')
 
 module.exports = {
     async index(req, res){
-        console.log(req.query)
+        
 
         const { latitude, longitude, techs} = req.query
         //Buscar todos devs num raio de 10 km
         //filtrar por tech
 
+        
         const techArray = parseStringAsArray(techs)
-
+        console.log(techArray)
+        
         const devs = await Dev.find({
             techs: {
                 $in:techArray
